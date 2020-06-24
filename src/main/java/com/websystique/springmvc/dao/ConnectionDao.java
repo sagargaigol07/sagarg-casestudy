543d214fbc224ec57e3131c8a250e15ca1b817c0
sagargaigol07 <sagar.g@cldcvr.com>
2020-06-24 17:08:23 +0530

    add source code in stagging

---
+++ src/main/java/com/websystique/springmvc/dao/ConnectionDao.java
@@ -0,0 +1,51 @@
+package com.websystique.springmvc.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.websystique.springmvc.model.User;
+
+public class ConnectionDao {
+	Connection con = null;
+	public Connection RetriveConnection() {
+	try { 
+		System.out.println("connection completed");
+		Class.forName("org.postgresql.Driver");  
+		con=DriverManager.getConnection(  
+		"jdbc:postgresql://java-application.cqgqd1oplv07.ap-south-1.rds.amazonaws.com:5432/test","postgres","javawebapp");
+		}catch(Exception e){
+		System.out.println("connection error");
+		System.out.println(e);}
+	return con;  
+	}
+	
+	public List<User> getUserList(Connection conn){
+		if(conn == null) {
+			conn = RetriveConnection();
+		}
+		List<User> userList = new ArrayList<>();
+		try {
+		Statement stmt=conn.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from userinfo");  
+		while(rs.next())  {
+			User user = new User();
+			user.setId(rs.getInt(1));
+			user.setUsername(rs.getString(2));
+			user.setAddress(rs.getString(3));
+			user.setEmail(rs.getString(4));
+			userList.add(user);
+		}
+		System.out.println("list is: "+userList.size());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return userList;
+		//con.close();  
+		
+	}
+
+}
